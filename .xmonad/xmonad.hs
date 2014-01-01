296b9a4798190d663e263024f278a7c3d65a9a9e
Naoki Mizuno <nm2372@gmail.com>
2014-01-01 10:43:13 -0500

    Add X related files

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,29 @@
+import XMonad
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
+-- spawnPipe
+import XMonad.Util.Run
+
+main = do
+    myStatusBar <- spawnPipe "xmobar"
+    xmonad defaultConfig {
+        terminal = "urxvt"
+        , borderWidth = 2
+        , normalBorderColor = "black"
+        , focusedBorderColor = "blue"
+
+        --, modMask = myModMask
+        , layoutHook = myLayoutHook
+        , manageHook = myManageHook
+        , logHook = myLogHook myStatusBar
+    }
+
+-- myModMask = mod4Mask
+
+myLayoutHook = avoidStruts $ layoutHook defaultConfig
+
+myManageHook = manageDocks <+> manageHook defaultConfig
+
+myLogHook h = dynamicLogWithPP xmobarPP {
+    ppOutput = hPutStrLn h
+}
