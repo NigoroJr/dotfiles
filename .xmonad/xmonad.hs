38e4826ffbb90433e52b87aaf6acb006777ec298
Naoki Mizuno <nigorojr@gmail.com>
2014-09-22 15:30:05 -0400

    Use custom keybindings so that it doesn't collide with Emacs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,14 @@ import XMonad.Actions.SpawnOn
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.SetWMName
-import XMonad.Util.Run
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Master
 import XMonad.Layout.PerWorkspace
+import XMonad.Util.Run
 
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+import System.Exit
 
 -- Terminal
 myTerminal :: String
@@ -75,6 +77,97 @@ myStartupHook = do
     spawnOn "4" "urxvt"
     spawnOn "4" "urxvt"
 
+-- Keybindings
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+
+    -- launch a terminal
+    [ ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+
+    -- launch dmenu_run
+    --, ((modMask,               xK_p     ), spawn "dmenu_run")
+    , ((modMask,               xK_q     ), spawn "dmenu_run")
+
+    -- close focused window
+    , ((modMask .|. shiftMask, xK_c     ), kill)
+
+     -- Rotate through the available layout algorithms
+    , ((modMask,               xK_space ), sendMessage NextLayout)
+
+    --  Reset the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
+
+    -- Resize viewed windows to the correct size
+    --, ((modMask,               xK_n     ), refresh)
+
+    -- Move focus to the next window
+    , ((modMask,               xK_Tab   ), windows W.focusDown)
+
+    -- Move focus to the next window
+    , ((modMask,               xK_j     ), windows W.focusDown)
+
+    -- Move focus to the previous window
+    , ((modMask,               xK_k     ), windows W.focusUp  )
+
+    -- Move focus to the master window
+    , ((modMask,               xK_m     ), windows W.focusMaster  )
+
+    -- Swap the focused window and the master window
+    , ((modMask,               xK_Return), windows W.swapMaster)
+
+    -- Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown  )
+
+    -- Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp    )
+
+    -- Shrink the master area
+    , ((modMask,               xK_h     ), sendMessage Shrink)
+
+    -- Expand the master area
+    , ((modMask,               xK_l     ), sendMessage Expand)
+
+    -- Push window back into tiling
+    , ((modMask,               xK_t     ), withFocused $ windows . W.sink)
+
+    -- Increment the number of windows in the master area
+    , ((modMask,               xK_comma ), sendMessage (IncMasterN 1))
+
+    -- Deincrement the number of windows in the master area
+    , ((modMask,               xK_period), sendMessage (IncMasterN (-1)))
+
+    -- Quit xmonad
+    , ((modMask .|. shiftMask, xK_q     ), io exitSuccess)
+
+    -- Restart xmonad
+    --, ((modMask,               xK_q     ), restart "xmonad" True)
+    ]
+    ++
+
+    --
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    --
+    [ ((m .|. modMask, k), windows $ f i)
+         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+    , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+    ]
+    ++
+
+    --
+    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+    --
+    [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
+    ]
+
+    -- ++
+    -- Spawn Vim
+    -- [ ((modMask,                 xK_r           ), spawn "gvim")
+    -- ]
+
 main = do
     myStatusBar <- spawnPipe "xmobar"
     xmonad $ defaultConfig {
@@ -87,4 +180,5 @@ main = do
         , manageHook = myManageHook
         , logHook = myLogHook myStatusBar
         , startupHook = myStartupHook
+        , keys = myKeys
     }
