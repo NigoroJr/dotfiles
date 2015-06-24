aa3450c5ff3df9428e7062089263c289c7f0b922
Naoki Mizuno <nigorojr@gmail.com>
2015-06-24 08:03:37 -0400

    Emacs: remap dispatch-alist keys for ace-window

--- .emacs.d/inits/20-ace-window.el
+++ .emacs.d/inits/20-ace-window.el
@@ -1,3 +1,10 @@
 ;;;; ace-window
 ;; Set before smartrep to enable repeatable C-x o
 (global-set-key (kbd "C-x o") 'ace-window)
+(defvar aw-dispatch-alist
+  '((?x aw-delete-window " Ace - Delete Window")
+    (?t aw-swap-window " Ace - Swap Window")
+    (?n aw-flip-window)
+    (?v aw-split-window-vert " Ace - Split Vert Window")
+    (?s aw-split-window-horz " Ace - Split Horz Window")
+    (?M delete-other-windows " Ace - Maximize Window")))
