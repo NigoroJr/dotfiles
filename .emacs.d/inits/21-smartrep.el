fd808c3cd626d12089099cf8ff4c4590dff288ab
Naoki Mizuno <nigorojr@gmail.com>
2015-06-27 19:03:24 -0400

    [Emacs] add C-x 0 to smartrep window operations group
    C-x 0 relates closely to the other C-x 1, 2, and 3 that are in the group, so
    it makes sense to be able to delete windows in the same sequence.

--- .emacs.d/inits/21-smartrep.el
+++ .emacs.d/inits/21-smartrep.el
@@ -4,6 +4,7 @@
 (smartrep-define-key global-map "C-x"
   '(("o" . other-window)
     ("O" . previous-multiframe-window)
+    ("0" . delete-window)
     ("1" . delete-other-windows)
     ("2" . split-window-below)
     ("3" . split-window-right)
