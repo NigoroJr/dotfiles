33759ccce9b38b4e42e930ea7ec4b02f14d790eb
Naoki Mizuno <nigorojr@gmail.com>
2016-10-04 21:12:19 +0900

    Remove unnecessary function call

--- emacs/emacs.d/inits/22-auto-complete-clang-async.el
+++ emacs/emacs.d/inits/22-auto-complete-clang-async.el
@@ -1,7 +1,7 @@
 ;; auto-complete-clang-async
 (require 'auto-complete-clang-async)
 (defun ac-cc-mode-setup ()
-  (setq ac-clang-complete-executable (expand-file-name "~/.emacs.d/el-get/auto-complete-clang-async/clang-complete"))
+  (setq ac-clang-complete-executable "~/.emacs.d/el-get/auto-complete-clang-async/clang-complete")
   (setq ac-sources (append '(ac-source-clang-async) ac-sources))
   (setq ac-clang-cflags '("-std=c++11"))
   (ac-clang-launch-completion-process))
