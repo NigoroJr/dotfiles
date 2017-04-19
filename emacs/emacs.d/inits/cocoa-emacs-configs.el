57dd0049ca0096c7fc63b8148d3bf4e085cc2441
Naoki Mizuno <nigorojr@gmail.com>
2017-04-19 12:57:38 +0900

    Disable message for exec-path-from-shell

--- emacs/emacs.d/inits/cocoa-emacs-configs.el
+++ emacs/emacs.d/inits/cocoa-emacs-configs.el
@@ -2,6 +2,7 @@
 (set-frame-font "RictyDiscord-13")
 
 ;;; exec-path-from-shell
+(setq exec-path-from-shell-check-startup-files nil)
 (exec-path-from-shell-initialize)
 (exec-path-from-shell-copy-envs
  '("SHELL"))
