c8530593cd041a7a42f05601e0fb6c669c0cd502
Naoki Mizuno <nigorojr@gmail.com>
2020-07-24 09:45:40 +0900

    Add ROS2 aliases and functions (no functions are defined for now)

--- zsh/rosrc.zsh
+++ zsh/rosrc.zsh
@@ -4,9 +4,14 @@ if (( ! $+commands[rosversion] )) && [[ ! -d /opt/ros ]]; then
     return
 fi
 
-source $HOME/.rosfunctions.zsh
-source $HOME/.rosaliases.zsh
+# ROS1 is installed
+if (( $+commands[roscore] )); then
+    source $HOME/.ros1functions.zsh
+    source $HOME/.ros1aliases.zsh
+    add-zsh-hook chpwd __source_ros
 
-add-zsh-hook chpwd __source_ros
-
-rossetup
+    rossetup
+else
+    source $HOME/.ros2functions.zsh
+    source $HOME/.ros2aliases.zsh
+fi
