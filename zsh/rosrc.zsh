876e148448224948b586a6b480130a1d82a19423
Naoki Mizuno <nigorojr@gmail.com>
2020-10-06 15:17:50 +0900

    Use ROS1 functions and aliases by default

--- zsh/rosrc.zsh
+++ zsh/rosrc.zsh
@@ -4,14 +4,14 @@ if (( ! $+commands[rosversion] )) && [[ ! -d /opt/ros ]]; then
     return
 fi
 
-# ROS1 is installed
-if (( $+commands[roscore] )); then
+# ROS2 is installed
+if (( $+commands[ros2] )); then
+    source $HOME/.ros2functions.zsh
+    source $HOME/.ros2aliases.zsh
+else
     source $HOME/.ros1functions.zsh
     source $HOME/.ros1aliases.zsh
     add-zsh-hook chpwd __source_ros
 
     rossetup
-elif (( $+commands[ros2] )); then
-    source $HOME/.ros2functions.zsh
-    source $HOME/.ros2aliases.zsh
 fi
