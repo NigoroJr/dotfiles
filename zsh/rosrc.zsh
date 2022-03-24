78be2fc59dc534e1b3afbd850bd089df9ff2559a
Naoki Mizuno <nigorojr@gmail.com>
2022-03-24 10:57:29 +0900

    Fix error on envs where ROS is not installed

--- zsh/rosrc.zsh
+++ zsh/rosrc.zsh
@@ -1,7 +1,7 @@
 # Utilities for ROS
 
 # Which ROS version to use (1, 2, or empty to disable)
-export USE_ROS_1_OR_2=2
+export USE_ROS_1_OR_2=1
 
 if (( ! $+commands[rosversion] )) && [[ ! -d /opt/ros ]]; then
     # ROS not installed
@@ -25,11 +25,13 @@ if (( $USE_ROS_1_OR_2 == 1 )); then
         fi
     done
 
-    source $HOME/.ros1functions.zsh
-    source $HOME/.ros1aliases.zsh
-    add-zsh-hook chpwd __source_ros
+    if [[ -n $_ros_version_name ]]; then
+        source $HOME/.ros1functions.zsh
+        source $HOME/.ros1aliases.zsh
+        add-zsh-hook chpwd __source_ros
 
-    rossetup $_ros_version_name
+        rossetup $_ros_version_name
+    fi
 elif (( $USE_ROS_1_OR_2 == 2 )); then
     for _version_name in ${(aO)_ros2_names[@]}; do
         if [[ -f /opt/ros/$_version_name/setup.zsh ]]; then
@@ -38,10 +40,12 @@ elif (( $USE_ROS_1_OR_2 == 2 )); then
         fi
     done
 
-    source /opt/ros/$_ros_version_name/setup.zsh
+    if [[ -n $_ros_version_name ]]; then
+        source /opt/ros/$_ros_version_name/setup.zsh
 
-    source $HOME/.ros2functions.zsh
-    source $HOME/.ros2aliases.zsh
+        source $HOME/.ros2functions.zsh
+        source $HOME/.ros2aliases.zsh
 
-    add-zsh-hook chpwd __source_ros
+        add-zsh-hook chpwd __source_ros
+    fi
 fi
