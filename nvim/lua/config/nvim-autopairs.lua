6af54061f29ac039cab52f393288b8ebcb3e02bd
Naoki Mizuno <nigorojr@gmail.com>
2024-07-21 00:27:28 +0900

    Enable fast wrap for autopairs

--- nvim/lua/config/nvim-autopairs.lua
+++ nvim/lua/config/nvim-autopairs.lua
@@ -1,4 +1,17 @@
 require("nvim-autopairs").setup({
   map_c_h = true,
   map_c_w = true,
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", "<", "'", '"', },
+    pattern = [=[[%'%"%>%]%)%}%,]]=],
+    end_key = "$",
+    before_key = "h",
+    after_key = "l",
+    cursor_pos_before = true,
+    keys = "abcdefghijklmnopqrstuvwxyz",
+    manual_position = true,
+    highlight = "Search",
+    highlight_grey="Comment"
+  },
 })
