8ce9842bf467fe466db04d0fa755b04f879d493e
Naoki Mizuno <nigorojr@gmail.com>
2024-07-21 12:07:04 +0900

    Oops, forgot to add some config files

---
+++ nvim/lua/config/colorful-winsep.lua
@@ -0,0 +1,8 @@
+require("colorful-winsep").setup({
+  hi = {
+    bg = "",
+    fg = "#6BB4E0",
+  },
+  no_exec_files = { "lazy", "TelescopePrompt", "mason", "CompetiTest" },
+
+})
---
+++ nvim/lua/config/jaq-nvim.lua
@@ -0,0 +1,73 @@
+require("jaq-nvim").setup({
+  cmds = {
+    -- Uses vim commands
+    internal = {
+      lua = "luafile %",
+      vim = "source %"
+    },
+
+    -- Uses shell commands
+    external = {
+      markdown = "glow %",
+      python   = "python3 %",
+      go       = "go run %",
+      sh       = "sh %"
+    }
+  },
+
+  behavior = {
+    -- Default type
+    default       = "terminal",
+
+    -- Start in insert mode
+    startinsert   = false,
+
+    -- Use `wincmd p` on startup
+    wincmd        = true,
+
+    -- Auto-save files
+    autosave      = true,
+
+    -- Close existing buffers
+    closeexisting = true
+  },
+
+  ui = {
+    float = {
+      -- See ':h nvim_open_win'
+      border    = "rounded",
+
+      -- See ':h winhl'
+      winhl     = "Normal",
+      borderhl  = "FloatBorder",
+
+      -- See ':h winblend'
+      winblend  = 0,
+
+      -- Num from `0-1` for measurements
+      height    = 0.8,
+      width     = 0.8,
+      x         = 0.5,
+      y         = 0.5
+    },
+
+    terminal = {
+      -- Window position
+      position = "vert",
+
+      -- Window size
+      size     = 80,
+
+      -- Disable line numbers
+      line_no  = false
+    },
+
+    quickfix = {
+      -- Window position
+      position = "vert",
+
+      -- Window size
+      size     = 80
+    }
+  }
+})
